3af4512ecd76e43526acce96b75ef278cb036182
hamid <65052613+hxmid@users.noreply.github.com>
2020-05-19 20:46:08 +1000

    added c# to github
    just added the actual winform into this project rather than having to make a whole other page (idk what else to call this)

---
+++ C#/Communicate with Arduino/RGB LED Controller.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Communicate_with_Arduino
+{
+    partial class RGB_LED_Controller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo_COMPort = new System.Windows.Forms.ComboBox();
+            this.button_update = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.combo_Profile = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // combo_COMPort
+            // 
+            this.combo_COMPort.FormattingEnabled = true;
+            this.combo_COMPort.Location = new System.Drawing.Point(113, 9);
+            this.combo_COMPort.Name = "combo_COMPort";
+            this.combo_COMPort.Size = new System.Drawing.Size(140, 21);
+            this.combo_COMPort.TabIndex = 0;
+            this.combo_COMPort.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(178, 288);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(75, 23);
+            this.button_update.TabIndex = 2;
+            this.button_update.Text = "Update";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COM Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Profile";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // combo_Profile
+            // 
+            this.combo_Profile.FormattingEnabled = true;
+            this.combo_Profile.Items.AddRange(new object[] {
+            "Off",
+            "Frictionless Rainbow",
+            "Custom"});
+            this.combo_Profile.Location = new System.Drawing.Point(113, 76);
+            this.combo_Profile.Name = "combo_Profile";
+            this.combo_Profile.Size = new System.Drawing.Size(140, 21);
+            this.combo_Profile.TabIndex = 7;
+            this.combo_Profile.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(113, 43);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(140, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RGB_LED_Controller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(263, 324);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.combo_Profile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.combo_COMPort);
+            this.Name = "RGB_LED_Controller";
+            this.Text = "RGB LED Controller";
+            this.Load += new System.EventHandler(this.RGB_LED_Controller_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combo_COMPort;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox combo_Profile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ C#/Communicate with Arduino/RGB LED Controller.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+
+namespace Communicate_with_Arduino
+{
+    public partial class RGB_LED_Controller : Form
+    {
+        const int MF_BYPOSITION = 0x400;
+        [DllImport("User32")]
+        private static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [DllImport("User32")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("User32")]
+        private static extern int GetMenuItemCount(IntPtr hWnd);
+
+        SerialPort port;
+        bool isConnected = false;
+        public RGB_LED_Controller()
+        {
+            InitializeComponent();
+            controls(isConnected);
+            string[] ports = SerialPort.GetPortNames();
+            numericUpDown1.Maximum = 1000000;
+            foreach (string thing in ports)
+            {
+                combo_COMPort.Items.Add(thing);
+                if (ports[0] != null)
+                {
+                    combo_COMPort.SelectedItem = ports[0];
+                }
+            }
+        }
+        private void connectToArduino()
+        {
+            isConnected = true;
+            string selectedPort = combo_COMPort.GetItemText(combo_COMPort.SelectedItem);
+            int baud_rate = Convert.ToInt32(Math.Round(numericUpDown1.Value));
+            port = new SerialPort(selectedPort, baud_rate, Parity.None, 8, StopBits.One);
+            port.Open();
+            button1.Text = "Disconnect";
+        }
+        private void RGB_LED_Controller_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+            disconnectFromArduino();
+        }
+        private void disconnectFromArduino()
+        {
+            isConnected = false;
+            port.Write("0");
+            port.Close();
+            button1.Text = "Connect";
+        }
+
+        private void RGB_LED_Controller_Load(object sender, EventArgs e)
+        {
+            IntPtr hMenu = GetSystemMenu(this.Handle, false);
+
+            int menuItemCount = GetMenuItemCount(hMenu);
+
+            RemoveMenu(hMenu, menuItemCount - 1, MF_BYPOSITION);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void controls(bool status)
+        {
+            button_update.Enabled = status;
+            combo_Profile.Enabled = status;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isConnected = !isConnected;
+            if (isConnected)
+                connectToArduino();
+            else
+                disconnectFromArduino();
+            controls(isConnected);
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            port.Write((combo_Profile.SelectedIndex).ToString());
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            disconnectFromArduino();
+            this.Close();
+        }
+    }
+}
